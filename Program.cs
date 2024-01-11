3daaf50a0ac200614aa6cf58ca45be42c1fdf0e4
adamishakad <adamishag1@gmail.com>
2024-01-11 13:26:57 -0800

    Updated Program

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, Brothers!");
+            Console.WriteLine("Great to see you all in this park, Spring is always the most beautiful Season of the year");
         }
     }
 }
