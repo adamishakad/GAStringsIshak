a4809d71b489ae33cee47eec699d0634a7f9b44e
adamishakad <adamishag1@gmail.com>
2024-01-11 12:35:27 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GAStringsIshak
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
