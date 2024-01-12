7be66e2d6212ce231d99d30d33ff515b9b86dbae
adamishakad <adamishag1@gmail.com>
2024-01-11 21:31:28 -0800

    Updated Program File

--- Program.cs
+++ Program.cs
@@ -6,6 +6,12 @@
         {
             Console.WriteLine("Hello, Brothers!");
             Console.WriteLine("Great to see you all in this park, Spring is always the most beautiful Season of the year");
+            Console.WriteLine("Adam");
+            Console.WriteLine("MyFirstName = Adam");
+            Console.WriteLine("MyLastName = Ab");
+            Console.WriteLine("MyFullName = myFirstName + myLastName");
+           
+
         }
     }
 }
